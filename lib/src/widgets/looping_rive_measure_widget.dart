26619418755b43633096cad3f28e1480df7bc3df
Papi-Eruh <alexis.deslandes@erudit.app>
2026-01-04 19:51:29 +0100

    Improve rive_measure_mixin

--- lib/src/widgets/looping_rive_measure_widget.dart
+++ lib/src/widgets/looping_rive_measure_widget.dart
@@ -1,33 +1,8 @@
-import 'dart:async';
-
 import 'package:anecdotes/anecdotes.dart';
 import 'package:anecdotes_catalog/src/models/models.dart';
 import 'package:anecdotes_catalog/src/widgets/widgets.dart';
 import 'package:flutter/material.dart' hide ValueChanged;
 import 'package:rive/rive.dart';
-
-class _FileLoaderFileSourceVisitor implements FileSourceVisitor<Future<File?>> {
-  @override
-  Future<File?> visitAssetSource(AssetSource source) {
-    return File.asset(source.path, riveFactory: Factory.rive);
-  }
-
-  @override
-  Future<File?> visitBytesSource(FutureBytesSource bytesAudioSource) async {
-    final bytes = await bytesAudioSource.bytesFuture;
-    return File.decode(bytes, riveFactory: Factory.rive);
-  }
-
-  @override
-  Future<File?> visitFilepathSource(FilepathSource source) {
-    return File.path(source.path, riveFactory: Factory.rive);
-  }
-
-  @override
-  Future<File?> visitNetworkSource(NetworkSource source) {
-    return File.url(source.url, riveFactory: Factory.rive);
-  }
-}
 
 /// A widget that displays a [LoopingRiveMeasure].
 ///
@@ -53,48 +28,17 @@ class _LoopingRiveMeasureWidgetState
           MeasureBaseWidget<LoopingRiveMeasure>
         >
     with RiveMeasureMixin {
-  late File _file;
-
-  bool _isInitialized = false;
-  final _completer = Completer<void>();
-
   @override
-  void initState() {
-    super.initState();
-    unawaited(initRive());
-  }
-
-  Future<void> initRive() async {
-    _file = (await widget.measure.riveSource.accept(
-      _FileLoaderFileSourceVisitor(),
-    ))!;
-    controller = RiveWidgetController(_file);
-    onSceneInit(controller);
-    setState(() => _isInitialized = true);
-    _completer.complete();
+  Widget buildContent(BuildContext context, RiveWidgetController controller) {
+    return RiveWidget(controller: controller, fit: Fit.cover);
   }
 
   @override
-  void dispose() {
-    _file.dispose();
-    controller.dispose();
-    super.dispose();
+  Widget buildLoading(BuildContext context) {
+    final surfaceColor = Theme.of(context).colorScheme.surface;
+    return ColoredBox(color: surfaceColor);
   }
 
   @override
-  Future<void> prepareBeforeReady() {
-    return _completer.future;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!_isInitialized) {
-      final surfaceColor = Theme.of(context).colorScheme.surface;
-      return ColoredBox(color: surfaceColor);
-    }
-    return RiveWidget(
-      controller: controller,
-      fit: Fit.cover,
-    );
-  }
+  FileSource get riveFileSource => widget.measure.riveSource;
 }
--- lib/src/widgets/rive_measure_mixin.dart
+++ lib/src/widgets/rive_measure_mixin.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:anecdotes/anecdotes.dart';
+import 'package:flutter/foundation.dart' hide Factory;
 import 'package:flutter/widgets.dart';
 import 'package:rive/rive.dart';
 
@@ -8,8 +11,14 @@ import 'package:rive/rive.dart';
 /// animation, including pausing, playing, and initializing the scene.
 mixin RiveMeasureMixin<M extends Measure, W extends MeasureBaseWidget<M>>
     on MeasureBaseState<M, W> {
+  late final File _file;
+
   /// The controller for the Rive animation widget.
   late RiveWidgetController controller;
+
+  final _readyCompleter = Completer<void>();
+
+  FileSource get riveFileSource;
 
   /// If true, the scene is already animated before being visible.
   ///
@@ -34,12 +43,73 @@ mixin RiveMeasureMixin<M extends Measure, W extends MeasureBaseWidget<M>>
     controller.active = true;
   }
 
-  /// Initializes the Rive scene.
-  ///
-  /// This method should be called when the Rive scene is initialized.
-  /// It sets the [controller] and its initial active state.
-  @mustCallSuper
-  void onSceneInit(RiveWidgetController controller) {
-    this.controller = controller..active = isInitActive;
+  @override
+  void initState() {
+    super.initState();
+    unawaited(_initRive());
+  }
+
+  Future<void> _initRive() async {
+    _file = (await riveFileSource.accept(
+      _FileLoaderFileSourceVisitor(),
+    ))!;
+    controller = RiveWidgetController(_file)..active = isInitActive;
+    _readyCompleter.complete();
+  }
+
+  @override
+  Future<void> prepareBeforeReady() {
+    return _readyCompleter.future;
+  }
+
+  Widget buildContent(
+    BuildContext context,
+    RiveWidgetController controller,
+  );
+
+  Widget buildLoading(BuildContext context);
+
+  @override
+  @nonVirtual
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _readyCompleter.future,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return buildContent(context, controller);
+        }
+        return buildLoading(context);
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _file.dispose();
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class _FileLoaderFileSourceVisitor implements FileSourceVisitor<Future<File?>> {
+  @override
+  Future<File?> visitAssetSource(AssetSource source) {
+    return File.asset(source.path, riveFactory: Factory.rive);
+  }
+
+  @override
+  Future<File?> visitBytesSource(FutureBytesSource bytesAudioSource) async {
+    final bytes = await bytesAudioSource.bytesFuture;
+    return File.decode(bytes, riveFactory: Factory.rive);
+  }
+
+  @override
+  Future<File?> visitFilepathSource(FilepathSource source) {
+    return File.path(source.path, riveFactory: Factory.rive);
+  }
+
+  @override
+  Future<File?> visitNetworkSource(NetworkSource source) {
+    return File.url(source.url, riveFactory: Factory.rive);
   }
 }
